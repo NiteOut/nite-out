8d578b6fcb3c2b8757aeb314d3ab7115223bf99d
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-08-28 11:46:51 -0700

    (feat) implemented movies api

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,7 +4,7 @@ module.exports = function(app, express) {
   var movieRouter = express.Router();
 
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(express.static(__dirname + '../../client'));
 
   app.use('/api/movies', movieRouter);
