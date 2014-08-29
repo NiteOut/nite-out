cae89434d9405d9026ab556333b1ed9f612c6c2f
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-08-29 12:02:00 -0700

    (feat) created database connection and updated documentation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 //////////////////////////////////////////
-// This file serves to start the server.
-// By exporting the app module we can
-// pass it to the middleware for routing
+// This file serves to start the server
+// and connect to the database.  By
+// exporting the app allows deferring to
+// to middleware for routing.
 //////////////////////////////////////////
 
 var express = require('express');
+var mongoose = require('mongoose');
+
+var dbURL = process.env.DBURL || 'mongodb://localhost/nite-out';
+mongoose.connect(dbURL);
 
 var app = express();
 
