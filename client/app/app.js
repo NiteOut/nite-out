6bc8a7552d5eb78ce8f6f149f9c13f32c19b9a18
Hack Reactor Students <accounts@hackreactor.com>
2014-08-28 19:29:12 -0700

    (feat) implemented main view

--- client/app/app.js
+++ client/app/app.js
@@ -2,17 +2,12 @@ angular.module('nite-out', [
   // 'nite-out.movies',
   // 'nite-out.map',
   'nite-out.main',
+  'nite-out.test',
   // 'nite-out.login',
   'ui.router'
 ])
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('main', {
-      url: '/main',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController'
-    });
   $urlRouterProvider.otherwise('/main');
 });
 
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,15 +1,19 @@
-angular.module('nite-out.main', [])
+angular.module('nite-out.main', ['ui.router'])
 
-.controller('MainController', function($scope) {
+
+.config(function($stateProvider) {
+  $stateProvider
+    .state('main', {
+      url: '/main',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController'
+    });
+})
+
+
+.controller('MainController', function($scope, $state) {
   $scope.input = '';
+  $scope.renderPage = function() {
+    $state.go('main.movies');
+  };
 });
-
-// .config(function($stateProvider) {
-//   $stateProvider
-//     .state('main', {
-//       url: '/main',
-//       templateUrl: 'app/main/main.html',
-//       controller: 'MainController'
-//     });
-// })
-
