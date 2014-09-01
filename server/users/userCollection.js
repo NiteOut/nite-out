6f32324296962d2401d963f31f855a6af98c8b4b
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 15:41:58 -0700

    (doc) added documentation for userCollection

--- server/users/userCollection.js
+++ server/users/userCollection.js
@@ -1,3 +1,6 @@
+// We must define a collection for bookshelf to handle all events
+// regarding to accessing one or more users.  This enables us to
+// update or save to the database.
 var db = require('../config/db.js');
 var User = require('./userModel.js');
 
