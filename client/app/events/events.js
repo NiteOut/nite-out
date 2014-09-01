d5ab190cc24a7af86ddec75ff05e82dbc0f83aa2
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 16:47:55 -0700

    (refactor) switched all events modules to array notation

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('nite-out.events', ['ui.router'])
-.config(function($stateProvider) {
+.config(['$stateProvider', function($stateProvider) {
   $stateProvider
     .state('main.events', {
       url: '/events',
       templateUrl: 'app/events/events.html',
       controller: 'EventsController'
     });
-})
+}])
 
 .controller('EventsController', ['$scope', 'Events', function($scope, Events){
   $scope.events = Events.events;
