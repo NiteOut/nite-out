762e5940b7fff708dc8db5f3ec060ddee6ff6e39
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 15:49:44 -0700

    (doc) updated userCollection documentation

--- server/users/userCollection.js
+++ server/users/userCollection.js
@@ -4,8 +4,11 @@
 var db = require('../config/db.js');
 var User = require('./userModel.js');
 
+// Define the new collection on the database, instantiated with the
+// User model
 var Users = new db.Collection({
   model: User
 });
 
+// Export the collection for use elsewhere
 module.exports = Users;
