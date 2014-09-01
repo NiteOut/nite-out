9c114d5aa4d89997737509b558a0705fad23eff8
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 15:40:16 -0700

    (doc) reworded eventRoutes.js and movieRoutes.js documentation to increase clarity

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -1,9 +1,9 @@
 // Defer to eventController methods for actual event handling.
-// Here we define all relevant routes on /api/events.
 var eventController = require('./eventController.js');
 
 module.exports = function(app) {
 
+  // Here we define all relevant routes on /api/events.
   app.route('/')
     .get(eventController.fetchEvents);
     
