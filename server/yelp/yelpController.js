412f5643c06c0559b319eec3b889cbfcb4fd9590
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 16:32:24 -0700

    (doc) added documentation for yelpController

--- server/yelp/yelpController.js
+++ server/yelp/yelpController.js
@@ -1,3 +1,5 @@
+// Yelp API is used in fetching secondary data upon user request.
+
 var Yelp = require('yelp');
 var keys = require('../../keys.js');
 
@@ -10,11 +12,24 @@ var yelp = Yelp.createClient({
 
 module.exports = {
   fetchBusiness: function(req, res, next) {
-    var name = req.query.name;
-    var location = req.query.location;
+    // Introduce proper formatting before making the API call.
+    // Space characters need to be represented as hyphens in order
+    // to be recognized by the API.
+    var name = req.query.name.replace(' ', '-');
+    var location = req.query.location.replace(' ', '-');
 
+    // Conduct API call and pass the data on to the user.
     yelp.business(name + '-' + location, function(error, data) {
-      res.json(data);
+      // Log the error on the server, and let the client know there was
+      // an error handling the request.
+      if(error) {
+        console.log(error);
+        res.writeHead(500);
+        res.send('There was an error processing your request');
+      } else {
+        // Return the business information resulting from the Yelp query
+        res.json(data);
+      }
     });
   }
 };
