90117b44d544b46d25f5cd230dc3bbf6a7fba2c2
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 15:20:52 -0700

    (doc) documentation added for eventRoutes.js

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -1,3 +1,5 @@
+// Import eventController methods for actual event handling.
+// Here we define all relevant routes on /api/events.
 var eventController = require('./eventController.js');
 
 module.exports = function(app) {
