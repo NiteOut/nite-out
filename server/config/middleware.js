08d50f6ae7c191198dd9141cb2346f5893b8f672
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 09:57:36 -0700

    (feat) updated middleware and added eventController and eventRoutes

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,24 +7,26 @@ var bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
 
-  // Each independent api will have its own router
-  // allowing us to easily manipulate the routes
-  // in the api without cluttering our middleware file
-  var movieRouter = express.Router();
-  var yelpRouter = express.Router();
-  var userRouter = express.Router();
-
   // Inject all middleware dependencies that will be used
   // in all routes
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(express.static(__dirname + '/../../client'));
 
+  // Each independent api will have its own router
+  // allowing us to easily manipulate the routes
+  // in the api without cluttering our middleware file
+  var movieRouter = express.Router();
+  var yelpRouter = express.Router();
+  var userRouter = express.Router();
+  var eventRouter = express.Router();
+
   // Define which routers are assigned to each route.
   // Wildcard is defined last in order to route to index
   app.use('/api/movies', movieRouter);
   app.use('/api/yelp', yelpRouter);
   app.use('/users', userRouter);
+  app.user('/api/events', eventRouter);
 
   app.get('/*', function(req, res) {
     res.redirect('/');
@@ -35,4 +37,5 @@ module.exports = function(app, express) {
   require('../movies/movieRoutes.js')(movieRouter);
   require('../yelp/yelpRoutes.js')(yelpRouter);
   require('../users/userRoutes.js')(userRouter);
+  require('../events/eventRoutes.js')(eventRouter);
 };
