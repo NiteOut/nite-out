d3a19b564822f3024bd315e5c8c78a1b0c01aa44
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 10:18:06 -0700

    (feat) fixed typo in middleware, added eventController method for fetching results

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -1,1 +1,27 @@
-var Eventbrite = require('eventbrite');
+var key = require('../../keys.js').eventApiKey;
+var Eventbrite = require('eventbrite')({
+  app_key: key
+});
+
+module.exports = {
+
+  fetchEvents: function(req, res, next) {
+    var params = {
+      within: 25,
+      keywords: req.query.keyword,
+      postal_code: req.query.zip,
+      category: req.query.category,
+      max: 25
+    };
+
+    Eventbrite.event_search(params, function(error, data) {
+      if (error) {
+        console.log(error);
+        res.send('An error occurred');
+      } else {
+        res.json(data);
+      }
+    });
+  }
+
+};
