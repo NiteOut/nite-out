4f48803f380050a13f1b92ea6a4464670a62dbc1
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 12:42:44 -0700

    (feat) naive implementation of event searching implemented

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,8 @@
 angular.module('nite-out', [
   'nite-out.main',
   'nite-out.movies',
-  // 'nite-out.map',
-  // 'nite-out.login',
+  'nite-out.events',
+  'nite-out.eventFactory',
   'nite-out.movieFactory',
 
   'nite-out.test',
