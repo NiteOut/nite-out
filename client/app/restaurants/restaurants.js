29999698cd4374b00a579460e3765905149f86f6
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-02 15:34:35 -0700

    (feat) progress made on map integration

--- client/app/restaurants/restaurants.js
+++ client/app/restaurants/restaurants.js
@@ -12,8 +12,10 @@ angular.module('nite-out.restaurants', ['ui.router'])
     });
 }])
 
-.controller('RestaurantsController', ['$scope', 'Restaurants', function($scope, Restaurants){
+.controller('RestaurantsController', ['$scope', 'Restaurants', 'Mapper', function($scope, Restaurants, Mapper){
   $scope.restaurants = Restaurants.restaurants;
+  // Populate our restaurants array from Opentable
+  Restaurants.getRestaurants(94102);
 
   // Query Yelp API for more information on our restaurant
   $scope.getInfo = function() {
@@ -22,7 +24,38 @@ angular.module('nite-out.restaurants', ['ui.router'])
     Restaurants.getInfo(data);
   };
 
-  // Populate our restaurants array from Opentable
-  Restaurants.getRestaurants(94102);
+
+  $scope.places = [];
+
+  // let $scope.map be the initial interface object for the google-map directive
+  // Mapper.init is the default object for setup
+  // for changing options go to: https://angular-ui.github.io/angular-google-maps/#!/api
+  $scope.map = Mapper.init;
+
+  // setting map options through the google-map directive interface
+  $scope.setCenter = function(restaurant){
+    // async Geocoder API call
+    var address = restaurant.address +', ' + restaurant.city;
+    Mapper.setCenter(address);
+  };
+
+  $scope.select = function(place){
+    $scope.setCenter(place.vicinity);
+    window.scrollTo(0,0);
+    console.log('you selected', place.name);
+  };
+
+  // takes args: ('type of establishment', optional radius in meters, optional callback())
+  $scope.renderListOfPlaces = function(){
+    $scope.places = Mapper.getLocations();
+  };
+
+  window.doStuff = function(){
+    $scope.$apply(function(){
+      $scope.places = Mapper.getLocations();
+    });
+    $scope.renderListOfPlaces();
+  };
+
 }]);
 
