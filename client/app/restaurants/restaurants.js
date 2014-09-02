faa126c358f9df2e258859649f9a6a0d0e86d4ed
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-02 13:43:44 -0700

    (doc) updated documentation for restaurants controller and factory

--- client/app/restaurants/restaurants.js
+++ client/app/restaurants/restaurants.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('nite-out.restaurants', ['ui.router'])
+
+// Configure out state using ui.router
 .config(['$stateProvider', function($stateProvider) {
   $stateProvider
     .state('main.restaurants', {
@@ -13,12 +15,14 @@ angular.module('nite-out.restaurants', ['ui.router'])
 .controller('RestaurantsController', ['$scope', 'Restaurants', function($scope, Restaurants){
   $scope.restaurants = Restaurants.restaurants;
 
+  // Query Yelp API for more information on our restaurant
   $scope.getInfo = function() {
     console.log(this.restaurant);
     var data = {name: this.restaurant.name, location: this.restaurant.city};
     Restaurants.getInfo(data);
   };
 
+  // Populate our restaurants array from Opentable
   Restaurants.getRestaurants(94102);
 }]);
 
--- client/app/restaurants/services.js
+++ client/app/restaurants/services.js
@@ -5,9 +5,14 @@ angular.module('nite-out.restaurantFactory', [])
 .factory('Restaurants', ['$http', function($http){
   var restaurants = [];
 
+  // Our query to Opentable API, API is not-official as Opentable does
+  // not currently have a public facing API.
   var getRestaurants = function(zipcode) {
+    // Restaurants array is spliced in order to clear it of previous entries.
     restaurants.splice(0);
     return $http({
+      // Conducted as a jsonp request in order to circumvent same-origin
+      // policy.
       method: 'jsonp',
       url: 'http://opentable.herokuapp.com/api/restaurants?callback=JSON_CALLBACK',
       params: {zip: zipcode},
@@ -19,6 +24,8 @@ angular.module('nite-out.restaurantFactory', [])
     });
   };
 
+  // Conduct our api call to the server.
+  // TODO: Handle the entire api call on the client-side.
   var getInfo = function(data) {
     return $http({
       method: 'GET',
@@ -26,10 +33,13 @@ angular.module('nite-out.restaurantFactory', [])
       params: data,
     })
     .success(function(response) {
-      console.log(response.url);
+      // TODO: Handle error responses more gracefully and only display
+      // meaningful responses.
+      console.log(response);
     });
   };
 
+  // Return an object filled with our shared methods and data.
   return {
     restaurants: restaurants,
     getRestaurants: getRestaurants,
