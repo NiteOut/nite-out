cfcfae514ee2a4143155bf7ca685c29d14af12ac
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-02 13:36:24 -0700

    (doc) added documentation for events controller and factory

--- client/app/events/services.js
+++ client/app/events/services.js
@@ -3,9 +3,15 @@
 angular.module('nite-out.eventFactory', [])
 
 .factory('Events', ['$http', function($http){
+  // Our factory holds all shared data returned from the server query
   var events = [];
 
   var getEvents = function(zipcode) {
+    // Events array is spliced in order to clear it of previous events.
+    events.splice(0);
+
+    // Conduct our api handled by the server.
+    // TODO: Handle the entire api call on the client-side.
     return $http({
       method: 'GET',
       url: '/api/events',
@@ -14,12 +20,14 @@ angular.module('nite-out.eventFactory', [])
       }
     })
     .then(function(res) {
+      // Push to events array to maintain reference instead of using =.
       res.data.results.forEach(function(item) {
         events.push(item);
       });
     });
   };
 
+  // Return an object filled with our shared methods and data.
   return {
     events: events,
     getEvents: getEvents
