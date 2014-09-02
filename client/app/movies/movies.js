93a9c7fec7b6d35fb387a6189b0db1d79438d09c
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-01 19:46:39 -0700

    (refactor) fully implemented build system for client distribution

--- client/app/movies/movies.js
+++ client/app/movies/movies.js
@@ -1,16 +1,18 @@
+'use strict';
+
 angular.module('nite-out.movies', ['ui.router'])
-.config(function($stateProvider) {
+.config(['$stateProvider', function($stateProvider) {
   $stateProvider
     .state('main.movies', {
       url: '/movies',
       templateUrl: 'app/movies/movies.html',
       controller: 'MoviesController'
     });
-})
+}])
 
-.controller('MoviesController', function($scope, Movies){
+.controller('MoviesController', ['$scope', 'Movies', function($scope, Movies){
   $scope.theaters = Movies.theaters;
 
   Movies.getTheaters(94102);
-});
+}]);
 
