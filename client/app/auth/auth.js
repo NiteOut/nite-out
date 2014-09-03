6a05c1a38c4f6f0ad1c754308da44587a1bd4f4c
jimjea <jimjea@gmail.com>
2014-09-03 13:36:39 -0700

    added ng-clicks to nested needLogin template

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,7 +1,12 @@
 angular.module('nite-out.auth', ['ui.router'])
 
 .controller('AuthController', ['$scope', function($scope) {
-  $scope.this= that
+  $scope.loginPage = function() {
+    console.log('login page')
+  };
+
+  $scope.signupPage = function() {
+    console.log('signup page')
+  };
+
 }])
-
-
