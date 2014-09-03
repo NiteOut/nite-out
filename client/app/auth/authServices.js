5cda007cf1d810d842f51d20e8a4d8243d45e8ad
jimjea <jimjea@gmail.com>
2014-09-03 14:39:33 -0700

    created http post request for users

---
+++ client/app/auth/authServices.js
@@ -0,0 +1,14 @@
+angular.module('nite-out.authServices',[])
+
+.factory('AuthRequests', ['$http', function($http) {
+  var loginInfo = function(userData) {
+    return $http({
+      method: 'POST',
+      url: '/users',
+      data: userData
+    })
+    .then(function(res) {
+      console.log(res.data);
+    });
+  };
+}]);
