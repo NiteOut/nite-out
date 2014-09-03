a7ff311e101c91ccca5e6269ce15878b66e35d7e
Hack Reactor Students <accounts@hackreactor.com>
2014-09-03 12:36:11 -0700

    created simple logged in directive

--- client/app/app.js
+++ client/app/app.js
@@ -12,15 +12,29 @@ angular.module('nite-out', [
   'nite-out.eventFactory',
   'nite-out.restaurants',
   'nite-out.restaurantFactory',
-
-  
-
-  //'nite-out.test',
   'ui.router'
 ])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/main');
-}]);
+}])
+
+
+
+// if logged in, then use this directive
+.directive('signedin', function() {
+  return {
+    restrict: 'EA',
+    replace: true,
+    // need to replace template URL
+    templateUrl: 'app/auth/loggedin.html',
+    // template: '<div>hello</div>',
+    link: function(scope, element, attr) {
+      console.log('working');
+    }
+  }
+});
+
+
 
 
