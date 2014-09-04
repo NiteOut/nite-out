71263ce7401428261a5aa2a59b36a5b879e39e2a
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-04 16:50:25 -0700

    (feat) added loading bar to all modules

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -13,6 +13,16 @@ angular.module('nite-out.events', ['ui.router'])
           return Events.getEvents(Search.current, Search.type);
         }
       },
+      data: {
+        loading: function() {
+          var el = angular.element(document.getElementById('main'));
+          el.html(
+            '<div class="spinner"><div class="rect1"></div>' +
+            '<div class="rect2"></div><div class="rect3"></div>' +
+            '<div class="rect4"></div><div class="rect5"></div></div>'
+            );
+        }
+      },
       controller: 'EventsController'
     });
 }])
