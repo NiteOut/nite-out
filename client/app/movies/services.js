ce3c78dfcc4ffd0ff5e478e5316a68fe9c4f343d
Kim Marquardt <kim.marquardt@gmail.com>
2014-09-04 15:12:47 -0700

    Map pins for movie results have numbers matching a list item's id

--- client/app/movies/services.js
+++ client/app/movies/services.js
@@ -23,7 +23,7 @@ angular.module('nite-out.movieFactory', [])
           phone: item.phoneNumber,
           address: item.address,
           movies: item.movies,
-          icon: '/assets/cinema.png',
+          icon: '/assets/numberedMarkers/number_'+(index+1)+'.png',
         };
         theaters.push(theater);
       });
