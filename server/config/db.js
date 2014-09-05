715966eac3c011a4d6bf8fe37e39270e34d91009
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-05 15:44:27 -0700

    (bug) fixed bug that caused user tokens to error in the database

--- server/config/db.js
+++ server/config/db.js
@@ -19,10 +19,9 @@ db.knex.schema.hasTable('users').then(function(exists) {
       user.string('password', 100);
       user.string('first_name', 100);
       user.string('last_name', 100);
-      user.string('token');
       user.timestamps();
     }).then(function() {
-      console.log('users created');
+      console.log('users table created');
     });
   }
 });
