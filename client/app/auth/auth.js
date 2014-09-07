de7161d15a1645e1006ae83b62fe945e4e94544c
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-07 12:45:25 -0700

    (doc) added documentation for auth controller

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,18 +2,39 @@
 
 angular.module('nite-out.auth', ['ui.router'])
 
+// Define our states for ui.router
+.config(['$stateProvider', function($stateProvider) {
+  $stateProvider
+    .state('login' , {
+      url: '/login',
+      templateUrl: 'app/auth/loginPage.html',
+      controller: 'AuthController'
+    })
+    .state('signup' , {
+      url: '/signup',
+      templateUrl: 'app/auth/signupPage.html',
+      controller: 'AuthController'
+    });
+}])
+
 .controller('AuthController', ['$scope', '$state', 'AuthRequests', function($scope, $state, AuthRequests) {
+  
+  // We handle which dialog to display here, based on which button is clicked.
   $scope.loginShown = false;
   $scope.signupShown = false;
 
+  // Login button clicked, display the login dialog
   $scope.toggleLogin = function() {
     $scope.loginShown = !$scope.loginShown;
   };
   
+  // Signup button clicked, display the signup dialog.  
   $scope.toggleSignup = function() {
     $scope.signupShown = !$scope.signupShown;
   };
 
+  // Here we handle passing data to the server, all business logic is handled in
+  // AuthRequests service.
   $scope.userInfo = {};
   $scope.loginStatus = AuthRequests.resolved;
 
@@ -28,18 +49,5 @@ angular.module('nite-out.auth', ['ui.router'])
   $scope.signout = function() {
     AuthRequests.signout();
   };
-}])
-
-.config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-    .state('login' , {
-      url: '/login',
-      templateUrl: 'app/auth/loginPage.html',
-      controller: 'AuthController'
-    })
-    .state('signup' , {
-      url: '/signup',
-      templateUrl: 'app/auth/signupPage.html',
-      controller: 'AuthController'
-    });
 }]);
+
