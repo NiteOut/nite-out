836a0209362c551fa9f2618bbdc062591159a145
jimjea <jimjea@gmail.com>
2014-09-08 13:51:30 -0700

    added thank you marketing page

--- client/app/checkout/marketTestingPage.js
+++ client/app/checkout/marketTestingPage.js
@@ -2,13 +2,13 @@ angular.module('nite-out.marketTest', ['ui.router'])
 
 .config(['$stateProvider', function($stateProvider) {
   $stateProvider
-    .state('main.markettest', {
+    .state('markettest', {
       url: '/thankyou',
-      templateUrl: 'app/checkout/marketTestPage.html',
+      templateUrl: 'app/checkout/marketTestingPage.html',
       controller: 'MarketController'
     });
 }])
 
 .controller('MarketController',['$scope', function($scope) {
-  
+
 }])
