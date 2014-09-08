ea7c837796a345139f0adcc198bfddb5c5b8fe26
jimjea <jimjea@gmail.com>
2014-09-08 13:28:48 -0700

    added marketing controller and html

--- client/app/checkout/marketTestingPage.js
+++ client/app/checkout/marketTestingPage.js
@@ -1,1 +1,14 @@
-marketTestingPage.js
+angular.module('nite-out.marketTest', ['ui.router'])
+
+.config(['$stateProvider', function($stateProvider) {
+  $stateProvider
+    .state('main.markettest', {
+      url: '/thankyou',
+      templateUrl: 'app/checkout/marketTestPage.html',
+      controller: 'MarketController'
+    });
+}])
+
+.controller('MarketController',['$scope', function($scope) {
+  
+}])
